ec8dce74d77ea2020dab6c55a783f327eff61995
Pete <petewhiteyorks@gmail.com>
2024-05-14 16:22:13 +0100

    removed comment

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -16,7 +16,6 @@ public class App
         System.out.println(sayHelloToSomeone("Ian"));
         System.out.println(sayHelloToSomeone("Pete"));
         System.out.println(sayHelloToSomeone("Victoria"));
-        // 
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
